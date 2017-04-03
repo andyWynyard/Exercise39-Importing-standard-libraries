1a6d6294c87fb3f540b3a45c0761beb087465e08
andyWynyard <andrew.wynyard@gmail.com>
2017-04-03 17:54:27 +0200

    final commit

--- PasswordDigest.java
+++ PasswordDigest.java
@@ -1,1 +1,24 @@
 import java.security.MessageDigest;
+import java.util.Scanner;
+import javax.xml.bind.DatatypeConverter;
+
+public class PasswordDigest {
+	public static void main(String[] args) throws Exception{
+			Scanner keyboard = new Scanner(System.in);
+			String pw, hash;
+
+			MessageDigest digest = MessageDigest.getInstance("SHA-512");
+
+			System.out.print("Password: ");
+			pw = keyboard.nextLine();
+
+			digest.update( pw.getBytes("UTF-8") );
+			hash = DatatypeConverter.printHexBinary(digest.digest() );
+
+			System.out.println( hash );
+	}	
+		/*SD
+		**Throws exception taken out destroys the code.
+		**I dont understand throws exception. */
+
+}
